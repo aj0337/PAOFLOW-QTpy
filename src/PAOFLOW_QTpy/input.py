2837c9f82b42dfda198ff69f21eb79af71d82ba8
Anooja Jayaraj <anoojajayaraj@gmail.com>
2023-02-07 20:13:08 +0100

    Added an input manager file

---
+++ src/PAOFLOW_QTpy/input.py
@@ -0,0 +1,14 @@
+from input_parameters import ConductorData
+from mpi4py import MPI
+
+
+def input_manager(filename, comm):
+
+    conductorData = ConductorData(filename, comm)
+    input_dict = conductorData.dict()
+
+    return input_dict
+
+
+#comm = MPI.COMM_WORLD
+#input_dict = input_manager('test.yaml', comm)
