577acdc92083c2e08dbcd10d087bdedce7c166d6
Anooja Jayaraj <anoojajayaraj@gmail.com>
2024-07-24 17:13:52 +0200

    Fix import in test

--- tests/test_write_header.py
+++ tests/test_write_header.py
@@ -2,13 +2,11 @@ from __future__ import annotations
 
 import pytest
 
-from PAOFLOW_QTpy.write_header import write_header
+from PAOFLOW_QTpy.io.write_header import write_header
 
 
 class TestWriteHeader:
-
     def test_long_messages(self):
-
         msg = "*" * 80
 
         with pytest.raises(ValueError) as _:
@@ -16,7 +14,6 @@ class TestWriteHeader:
 
     @pytest.mark.parametrize("msg", ["hello", "goodbye"])
     def test_output(self, capfd, msg):
-
         write_header(msg)
 
         out, _ = capfd.readouterr()
