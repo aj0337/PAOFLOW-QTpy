22bbc4f473a175ceb6d8fa77a81ed193ffe73ea7
Anooja Jayaraj <anoojajayaraj@gmail.com>
2024-07-24 17:14:11 +0200

    Comment out old code

--- src/PAOFLOW_QTpy/smearing_base.py
+++ src/PAOFLOW_QTpy/smearing_base.py
@@ -1,26 +1,26 @@
-import numpy as np
+# import numpy as np
 
 
-def gaussian(x):
-    cost /= np.sqrt(np.pi)
-    return cost * np.exp(-x**2)
+# def gaussian(x):
+#     cost /= np.sqrt(np.pi)
+#     return cost * np.exp(-x**2)
 
 
-def metpax(x):
-    cost /= np.sqrt(np.pi)
-    return cost * np.exp(-x**2) * (1.5 - x**2)
+# def metpax(x):
+#     cost /= np.sqrt(np.pi)
+#     return cost * np.exp(-x**2) * (1.5 - x**2)
 
 
-def marzarivanderbilt(x):
-    cost /= np.sqrt(np.pi)
-    return cost * np.exp(-(x - 1.0 / np.sqrt(2))**2) * (2.0 - np.sqrt(2) * x)
+# def marzarivanderbilt(x):
+#     cost /= np.sqrt(np.pi)
+#     return cost * np.exp(-(x - 1.0 / np.sqrt(2))**2) * (2.0 - np.sqrt(2) * x)
 
 
-def fermidirac(x):
-    cost /= 2.0
-    return cost * 1.0 / (1.0 + np.cosh(x))
+# def fermidirac(x):
+#     cost /= 2.0
+#     return cost * 1.0 / (1.0 + np.cosh(x))
 
 
-def lorentzian(x):
-    cost /= np.pi
-    return cost * 1.0 / (1.0 + x**2)
+# def lorentzian(x):
+#     cost /= np.pi
+#     return cost * 1.0 / (1.0 + x**2)
