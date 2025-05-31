3089f467132cdd987ba0b840ebd3901bcad80b59
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-05-31 10:12:35 +0200

    Move logic from deleted `Conductor` class to main

--- examples/example1/main.py
+++ examples/example1/main.py
@@ -1,7 +1,36 @@
-from PAOFLOW_QTpy.conductor import Conductor
+from PAOFLOW_QTpy.io.startup import startup
+from PAOFLOW_QTpy.io.write_header import write_header
+from PAOFLOW_QTpy.parsers.atmproj_tools import parse_atomic_proj
+from PAOFLOW_QTpy.io.summary import print_summary
+
+
+def main():
+    file_proj = "./al5.save/atomic_proj.xml"
+    work_dir = "./al5.save"
+    prefix = "al5"
+    postfix = "_bulk"
+    atmproj_sh = 3.5
+    atmproj_thr = 0.9
+    atmproj_nbnd = 60
+    atmproj_do_norm = False
+    do_orthoovp = True
+
+    startup("conductor.py")
+    write_header("Conductor Initialization")
+    parse_atomic_proj(
+        file_proj=file_proj,
+        work_dir=work_dir,
+        prefix=prefix,
+        postfix=postfix,
+        atmproj_sh=atmproj_sh,
+        atmproj_thr=atmproj_thr,
+        atmproj_nbnd=atmproj_nbnd,
+        atmproj_do_norm=atmproj_do_norm,
+        do_orthoovp=do_orthoovp,
+        write_intermediate=True,
+    )
+    print_summary()
 
 
 if __name__ == "__main__":
-    simulation = Conductor()
-    simulation.work_dir = "./al5.save"
-    simulation.run()
+    main()
