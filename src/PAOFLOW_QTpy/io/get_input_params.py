1e5002ac400e4bb1e8fa310e5b79b97f1f55bd50
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-06-01 08:59:59 +0200

    Populate dictionar with default values if not provided in the input

--- src/PAOFLOW_QTpy/io/get_input_params.py
+++ src/PAOFLOW_QTpy/io/get_input_params.py
@@ -1,6 +1,19 @@
 import yaml
+from pathlib import Path
+from PAOFLOW_QTpy.io.input_parameters import ConductorData
 
 
-def get_input_from_yaml(yaml_path: str) -> dict:
-    with open(yaml_path) as f:
-        return yaml.safe_load(f)
+def get_input_from_yaml(yaml_file: str) -> dict:
+    with open(yaml_file) as f:
+        content = f.read()
+        return yaml.safe_load(content)
+
+
+def load_summary_data_from_yaml(yaml_path: str, comm=None) -> dict:
+    user_input = get_input_from_yaml(yaml_path)
+    input_conductor = user_input.get("input_conductor", {})
+    dummy_filename = Path(yaml_path).absolute()
+    defaults = ConductorData(filename=dummy_filename, comm=comm).model_dump()
+    summary_data = {**defaults, **input_conductor}
+
+    return summary_data
