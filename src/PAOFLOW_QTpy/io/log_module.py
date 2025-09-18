e31edec2e2b724a4381e1e676c043b50f2d1c961
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-09-18 08:55:27 +0200

    Remove unused `log_push` function

--- src/PAOFLOW_QTpy/io/log_module.py
+++ src/PAOFLOW_QTpy/io/log_module.py
@@ -1,26 +1,8 @@
-import logging
-from datetime import datetime
-import psutil
-
 from mpi4py import MPI
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
-
-# Initialize the logger
-logger = logging.getLogger("my_logger")
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler("logfile.log")
-formatter = logging.Formatter("%(message)s")
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-# Global variables
-debug_mode = True  # Assuming this is set somewhere in your application
-allocated = True  # Assuming this is set somewhere in your application
-stack = []
-stack_maxdim = 100  # Define the maximum stack size
-stack_index = 0
+size = comm.Get_size()
 
 
 def log_rank0(message: str):
@@ -28,37 +10,9 @@ def log_rank0(message: str):
         print(message)
 
 
-def log_push(name):
-    global stack, stack_index
+def log_section_start(name: str):
+    log_rank0(f"Begins {name}")
 
-    # Check debug mode and allocation status
-    if not debug_mode or not allocated:
-        return
 
-    # Update stack index and check bounds
-    stack_index += 1
-    if stack_index > stack_maxdim:
-        return
-
-    # Update stack with the new name
-    if stack_index >= len(stack):
-        stack.append("")
-    stack[stack_index] = name.strip()
-
-    # Get current date and time
-    now = datetime.now()
-    cdate = now.strftime("%Y-%m-%d")
-    ctime = now.strftime("%H:%M:%S")
-
-    # Get memory usage
-    memory = psutil.Process().memory_info().rss // 1024  # Memory in KB
-
-    # Format the log entry
-    log_entry = (
-        f"{cdate} {ctime} | {memory} KB | "
-        + "..| " * (stack_index - 1)
-        + stack[stack_index]
-    )
-
-    # Write to the log file
-    logger.info(log_entry)
+def log_section_end(name: str):
+    log_rank0(f"Ends {name}")
