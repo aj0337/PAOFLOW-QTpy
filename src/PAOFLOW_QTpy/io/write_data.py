Fix sign issues in writing to xml files to match Fortran output

src/PAOFLOW_QTpy/io/write_data.py
@@ -517,8 +517,8 @@ def write_operator_xml(
                         f'    <VR.{ir + 1} type="complex" size="{total_elements}">\n'
                     )
 
-                    for i in range(rows):
-                        for j in range(cols):
+                    for j in range(cols):
+                        for i in range(rows):
                             val = matrix[i, j]
                             f.write(f"{val.real: .15E},{val.imag: .15E}\n")
 
@@ -576,6 +576,7 @@ def write_kresolved_operator_xml(
     )
 
     write_operator_xml(
+        output_dir=Path(filename).parent,
         filename=filename,
         operator_matrix=operator_k[np.newaxis, ...],
         ivr=ivr_k,

src/PAOFLOW_QTpy/parsers/atmproj_tools.py
@@ -399,7 +399,8 @@ def build_scheme1(
 
         H += (energy - opts.sh) * np.outer(proj_b, proj_b.conj())
 
-    return 0.5 * (H + H.conj().T)
+        H = 0.5 * (H + H.conj().T)
+    return H.T
 
 
 def build_scheme2(
@@ -436,7 +437,8 @@ def build_scheme2(
     IPA = inv(PA)
     H_aux = (E - opts.sh * IPA) @ A.conj().T
     H = A @ H_aux
-    return 0.5 * (H + H.conj().T)
+    H = 0.5 * (H + H.conj().T)
+    return H.T
 
 
 def apply_overlap_transformation(H: np.ndarray, S: np.ndarray) -> np.ndarray: