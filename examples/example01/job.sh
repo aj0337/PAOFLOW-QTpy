d555a7ed47822904e23560486c8074bd787c5b89
Anooja Jayaraj <anoojajayaraj@gmail.com>
2025-09-24 15:46:51 +0200

    Add log files of working code

--- examples/example01/job.sh
+++ examples/example01/job.sh
@@ -3,7 +3,8 @@
 PW_EXEC="/home/anooja/Work/tools/qe-5.3/PW/src/pw.x"
 PP_EXEC="/home/anooja/Work/tools/qe-5.3/PP/src/projwfc.x"
 
-"$PW_EXEC" <scf.in >scf.out
-"$PW_EXEC" <nscf.in >nscf.out
-"$PP_EXEC" <proj.in >proj.out
+# "$PW_EXEC" <scf.in >scf.out
+# "$PW_EXEC" <nscf.in >nscf.out
+# "$PP_EXEC" <proj.in >proj.out
+rm -rf output/
 python main.py conductor.yaml > conductor.out
