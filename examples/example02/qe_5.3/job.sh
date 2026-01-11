b6acbb57620624b91842e435900183e37398732e
Anooja Jayaraj <anoojajayaraj@gmail.com>
2026-01-11 11:02:01 +0100

    Move example to v5.3 directory

--- examples/example02/qe_5.3/job.sh
+++ examples/example02/qe_5.3/job.sh
@@ -6,7 +6,7 @@ PP_EXEC="/home/anooja/Work/tools/qe-5.3/PP/src/projwfc.x"
 # "$PW_EXEC" <scf.in >scf.out
 # "$PW_EXEC" <nscf.in >nscf.out
 # "$PP_EXEC" <proj.in >proj.out
-rm -rf output/
+# rm -rf output/
 python main_conductor.py conductor_bulk.yaml > conductor_bulk.out
 python main_conductor.py conductor_lcr.yaml > conductor_lcr.out
 python main_conductor.py conductor_lead_Al.yaml > conductor_lead_Al.out
